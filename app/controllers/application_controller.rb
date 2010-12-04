8a2d3e69bddea56e5421aa69777f133459008e82
Michael Young <mikeyoung13@gmail.com>
2010-12-04 07:54:24 -0500

    added time to left sidebar (extra credit)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :prepare_time_for_display
+  def prepare_time_for_display
+    @current_time = Time.now
+  end
+  
 end
