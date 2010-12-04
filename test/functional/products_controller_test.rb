c9f7bf869a068fa1bfdc4c102b051f754f666fcb
Michael Young <mikeyoung13@gmail.com>
2010-12-04 08:08:40 -0500

    added functional test for product controller (extra credit)

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    
+    assert_select '#columns #side a', :minimum => 4
+    assert_select '#main #product_list dt', 3
+    assert_select '#main #product_list dt', 'Programming Ruby 1.9'
+    
   end
 
   test "should get new" do
