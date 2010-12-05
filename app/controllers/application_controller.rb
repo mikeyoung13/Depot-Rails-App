aeb6d8eb9a302e52c3922121b87408de4ec46052
Michael Young <mikeyoung13@gmail.com>
2010-12-05 18:30:19 -0500

    basic cart working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  private
+
+  def current_cart 
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+   
   before_filter :prepare_time_for_display
   def prepare_time_for_display
     @current_time = Time.now
