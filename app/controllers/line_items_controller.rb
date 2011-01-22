608ca231077c71c4c753208ede1a4a7766959e21
Michael Young <mikeyoung13@gmail.com>
2011-01-21 20:32:51 -0500

    iteration F3 (Highlight changes)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -50,7 +50,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) } 
-        format.js
+        format.js   { @current_item = @line_item }
         #format.html { redirect_to(@line_item.cart) }        
         #format.html { redirect_to(@line_item, :notice => 'Line item was successfully created.') }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
