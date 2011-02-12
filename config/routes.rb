e511df16512ac4cddcd0d7aea3fd008e7b978305
Michael Young <mikeyoung13@gmail.com>
2011-02-12 08:11:09 -0500

    Checkout G2 - Atom Feed

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Depot::Application.routes.draw do
   get "store/index"
 
   resources :products
+  
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
